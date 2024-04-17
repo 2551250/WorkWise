fd96d879d5b1bf3546d276e320c03a79cd0cbc28
Tania908 <2486457@students.wits.ac.za>
2024-04-17 16:34:08 +0200

    Resolving LoginForm issues #1

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -11,28 +11,7 @@ const LoginForm = () => {
     const [passwordError, setPasswordError] = useState("");
 
     const onButtonClick = () => {
-        setEmail("");
-        setPassword("");
-
-        if (email === ""){
-            setEmailError("Please enter your email");
-            return
-        }
-
-        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-            setEmailError('Please enter a valid email')
-            return
-        }
-    
-        if ('' === password) {
-        setPasswordError('Please enter a password')
-        return
-        }
-    
-        if (password.length < 7) {
-        setPasswordError('The password must be 8 characters or longer')
-        return
-        }
+        //TODO: Implement logging in functionality
     }
 
     return (
