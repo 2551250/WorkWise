6abff1d02d1d8e6b34074f212b7e41e3eeffa8ba
Tania908 <2486457@students.wits.ac.za>
2024-04-17 16:33:15 +0200

    Resolving LoginForm issues #1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import { login } from "./backend.js";
+import LoginForm from './Components/LoginForm/LoginForm.jsx';
 
-function LoginButton() {
-  return (
-    <button type="button" onClick={login}>Login</button>
-  )
-}
 
 function App() {
   return (
-    <div>
-      <LoginButton />
-    </div>
+      <LoginForm />
   );
 }
 
--- src/backend.test.js
+++ src/backend.test.js
@@ -1,12 +1,17 @@
-import { employeeExists } from "./backend";
+import { employeeExists, getRole} from "./backend";
 
 const data = [
     {
         "EMAIL": "gcheadle@workwise.co.za",
-        "PASSWORD": "password"
+        "PASSWORD": "password",
+        "ROLE": "HR"
     }
 ];
 
-test('checks login', () =>{
+test('checks employee exists', () =>{
     expect(employeeExists("gcheadle@workwise.co.za", "password",data)).toBe(true);
 });
+
+test("checks get role", ()=>{
+    expect(getRole("gcheadle@workwise.co.za", "password",data)).toBe("HR");
+})
