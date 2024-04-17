cb69b846108f0d0bce0f3a5ae936506513f215b5
Tania908 <2486457@students.wits.ac.za>
2024-04-17 16:17:57 +0200

    Removed conflicting Login Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
-import { login } from "./backend.js";
 import LoginForm from './Components/LoginForm/LoginForm.jsx';
 
-function LoginButton() {
-  return (
-    <button type="button" onClick={login}>Login</button>
-  )
-}
 
 function App() {
   return (
