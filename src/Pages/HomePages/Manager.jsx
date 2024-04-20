50797b93624987344070cfee7f6c99bbff960e53
NtandoRaji <2584925@students.wits.ac.za>
2024-04-20 17:34:33 +0200

    added homepages & cards

--- src/Pages/HomePages/Manager.jsx
+++ src/Pages/HomePages/Manager.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import "./Manager.css";
+
+import burgerIcon from "../../Assets/burger-icon.svg";
+import feedbackIcon from "../../Assets/feedback-icon.svg";
+import peopleIcon from "../../Assets/people-icon.svg";
+import timesheetIcon from "../../Assets/timesheet-icon.svg";
+
+import Card from "../../Components/Card/Card";
+
 
 function Manager(){
     return(
-        <h1>Manager Dashboard </h1>
+        <>
+    <header className="heading">
+            <h1>WorkWise</h1>
+            <button className="logoutButton">Log Out</button>
+        </header>
+        <main className="homepage">
+            <Card title="Timesheets" imgSrc={timesheetIcon}/>
+            <Card title="Feedback" imgSrc={feedbackIcon}/>
+            <Card title="Plan Meals" imgSrc={burgerIcon}/>
+            <Card title="View Staff" imgSrc={peopleIcon}/>
+        </main>
+        </>
     );
 }
 
