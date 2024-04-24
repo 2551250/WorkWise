eba3713adce7c8b2aab2dadd7a44c5ed939ee678
NtandoRaji <2584925@students.wits.ac.za>
2024-04-24 18:45:47 +0200

    Created Project Pages

---
+++ src/Pages/ProjectPages/ManagerProjectPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const ManagerProjectPage = () => {
+    return (
+        <>
+            <h1>Welcome to the Manager Project Page</h1>
+        </>
+    );
+}
+    
+export default ManagerProjectPage;
---
+++ src/Pages/ProjectPages/StaffProjectPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const StaffProjectPage = () => {
+    return (
+        <>
+            <h1>Welcome to the Manager Project Page</h1>
+        </>
+    );
+}
+
+export default StaffProjectPage;
