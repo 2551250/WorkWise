3c6e62784cda52a11907a882cff8b8e0708a09d4
NtandoRaji <2584925@students.wits.ac.za>
2024-04-24 18:27:02 +0200

    Updated Manager page to latest design

--- src/Pages/HomePages/Manager.jsx
+++ src/Pages/HomePages/Manager.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Manager.css";
+import { useNavigate } from "react-router-dom";
 
+// Loading Card Icons from Assets Folder
 import burgerIcon from "../../Assets/burger-icon.svg";
-import feedbackIcon from "../../Assets/feedback-icon.svg";
+import projectIcon from "../../Assets/project-icon.svg";
 import peopleIcon from "../../Assets/people-icon.svg";
 import timesheetIcon from "../../Assets/timesheet-icon.svg";
 
@@ -10,6 +12,16 @@ import Card from "../../Components/Card/Card";
 
 
 function Manager(){
+    // Variables
+    const navigate = useNavigate();
+
+    // Functions & Logic
+    // empty function for unimplemented data
+    const empty = () => {
+
+    }
+
+    // HTML Code
     return(
         <>
     <header className="heading">
@@ -17,10 +29,10 @@ function Manager(){
             <button className="logoutButton">Log Out</button>
         </header>
         <main className="homepage">
-            <Card title="Timesheets" imgSrc={timesheetIcon}/>
-            <Card title="Feedback" imgSrc={feedbackIcon}/>
-            <Card title="Plan Meals" imgSrc={burgerIcon}/>
-            <Card title="View Staff" imgSrc={peopleIcon}/>
+            <Card title="Timesheets" imgSrc={timesheetIcon} onClick={empty}/>
+            <Card title="Projects" imgSrc={projectIcon} onClick={empty}/>
+            <Card title="Plan Meals" imgSrc={burgerIcon} onClick={empty}/>
+            <Card title="View Staff" imgSrc={peopleIcon} onClick={empty}/>
         </main>
         </>
     );
