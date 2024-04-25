1bf44d8b3e59e97935fd41d1ea9d08e530597e14
NtandoRaji <2584925@students.wits.ac.za>
2024-04-26 00:08:44 +0200

    Implemented Manager Project Page

--- src/Pages/ProjectPages/ManagerProjectPage.jsx
+++ src/Pages/ProjectPages/ManagerProjectPage.jsx
@@ -1,13 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "../../Components/Header/Header";
+import ViewProjectCard from "../../Components/ViewProjectCard/ViewProjectCard";
+import { getAllProjects } from "../../backend";
+
+import "./ManagerProjectPage.css";
+
+
+// TODO: comment code!!!
+const LoadViewProjectsSection = ({projects}) => {
+    return (
+        <section className="view-project">
+            <h2>View Projects</h2>
+            {projects.map((project) => (
+                <ViewProjectCard key={project.PROJECT_ID} name={project.PROJECT_NAME} description={project.DESCRIPTION} estimatedTime={project.ESTIMATED_TIME} members={["Member 1", "Member 2", "Member 3", "Member 4"]}/>
+            )
+            )}
+        </section>
+    );
+}
+
+
+const LoadAddProjectsSection = () => {
+
+    return (
+        <section className="view-project">
+            <h2>Add Project</h2>
+        </section>
+    );
+}
+
 
 const ManagerProjectPage = () => {
+    // Variable
+
+    const [viewProjects, setViewProjects] = useState(true);
+    const [projects, setProjects] = useState([]);
+
+    // Functions & Logic
+    useEffect(() => {
+        getAllProjects()
+        .then((data) => {
+            setProjects(data)
+        })
+        .catch((errorMessage) => {
+            console.error(errorMessage);
+        });
+    }, []);
+
+    //TODO: comment code!!!
+    const ViewProjectsButtonClicked = () => {
+        setViewProjects(true);
+    }
+
+    const AddProjectButtonClicked = () => {
+        setViewProjects(false);
+    }
+
+    // HTML Code
     return (
         <>
             <Header>
-                <h1> Projects - Manager </h1>
-            </Header>
+                <h1> Workwise </h1>
+                <button className="logoutButton">Log Out</button>
+            </Header> 
+
+            <section className="display">
+                <section className="panel">
+                    <button className="panelButtons" onClick={ViewProjectsButtonClicked}> View Projects</button>
+                    <button className="panelButtons" onClick={AddProjectButtonClicked}>Add a Project</button>
+                </section>
+
+                {viewProjects 
+                 ? <LoadViewProjectsSection projects={projects}/> 
+                 : <LoadAddProjectsSection/>
+                }
+                
+            </section>
         </>
     );
 }
