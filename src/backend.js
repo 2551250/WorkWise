3711e04446fa6834ff9bbe002c4a17ed8dcae397
2551250 <2551250@students.wits.ac.za>
2024-04-25 17:32:22 +0200

    Try to fix build failure

--- src/backend.js
+++ src/backend.js
@@ -1,5 +1,4 @@
 
-import axios from 'axios'
 const URL = "https://workwise-backend.azurewebsites.net"
 //Returns all employees in the database
 async function getAllEmployees() {
@@ -82,20 +81,5 @@ async function getManagerProjects(managerID) {
   }
 }
 
-async function insertProject(project_name, description, manager_id, estimated_time) {
-  const project = {
-    project_name: project_name,
-    description: description,
-    manager_id: manager_id,
-    estimated_time: estimated_time
-  }
-
-  axios.post(`${URL}/Project`, project)
-    .then((res) => {
-      return console.log(res.data);
-    }).catch((err) => {
-      return console.error(err);
-    });
-}
 // exports
-export { getRole, getAllEmployees, getAllProjects, getStaffProjects, getManagerProjects, getProjectAssignedStaff, insertProject }
+export { getRole, getAllEmployees, getAllProjects, getStaffProjects, getManagerProjects, getProjectAssignedStaff }
