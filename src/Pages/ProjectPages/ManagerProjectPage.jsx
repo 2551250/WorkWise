38109694790b8e17e0c2f0fb345f006a8bc84fd9
NtandoRaji <2584925@students.wits.ac.za>
2024-04-26 09:02:20 +0200

    implemented add project section

--- src/Pages/ProjectPages/ManagerProjectPage.jsx
+++ src/Pages/ProjectPages/ManagerProjectPage.jsx
@@ -2,13 +2,26 @@ import React, { useEffect, useState } from "react";
 
 import Header from "../../Components/Header/Header";
 import ViewProjectCard from "../../Components/ViewProjectCard/ViewProjectCard";
-import { getAllProjects } from "../../backend";
+import { getManagerProjects } from "../../backend";
+import { insertProject } from "../../backend_post_requests";
 
 import "./ManagerProjectPage.css";
 
 
 // TODO: comment code!!!
-const LoadViewProjectsSection = ({projects}) => {
+const ViewProjectsSection = ({ managerID }) => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        getManagerProjects(managerID)
+        .then((data) => {
+            setProjects(data)
+        })
+        .catch((errorMessage) => {
+            console.error(errorMessage);
+        });
+    }, []);
+
     return (
         <section className="view-project">
             <h2>View Projects</h2>
@@ -21,32 +34,96 @@ const LoadViewProjectsSection = ({projects}) => {
 }
 
 
-const LoadAddProjectsSection = () => {
+// TODO: comment code!!!
+const AddProjectsSection = ({ managerID, setViewProjects}) => {
+    const [projectName, setProjectName] = useState("");
+    const [projectDescription, setProjectDescription] = useState("");
+    const [projectEstimatedTime, setProjectEstimatedTime] = useState(0);
+    const [projectMembers, setProjectsMembers] = useState([]);
+
+    const handleButtonClick = () => {
+        //TODO: Implement input validation
+
+        // TODO: Find way to get members assigned to project
+        setProjectsMembers(["Member 1", "Member 2", "Member 3", "Member 4"])
+        // console.log(projectName, projectDescription, projectEstimatedTime, projectMembers);
+
+        insertProject(projectName, projectDescription, managerID, projectEstimatedTime);
+        setViewProjects(true);
+    }
 
     return (
-        <section className="view-project">
-            <h2>Add Project</h2>
-        </section>
+        <section className="add-project">
+        <h2>Add a Project</h2>
+       
+        <article className="formatting">
+            <p className = "labels">Name</p>
+            <input
+                value = {projectName} 
+                className="input-name" 
+                type="text" 
+                placeholder="Enter a project name"
+                onChange={(event) => {setProjectName(event.target.value)}}
+            />
+        </article>
+       
+        <article className="formatting">
+            <p className = "labels">Description</p>
+            <textarea 
+                value = {projectDescription}
+                maxLength="200" 
+                rows="5" 
+                placeholder="Enter a description"
+                onChange={(event) => {setProjectDescription(event.target.value)}}
+            >    
+            </textarea>
+        </article>
+
+        <article className="formatting">
+            <p className = "labels">Estimated Time</p>
+            <input 
+                value={projectEstimatedTime}
+                className="input-time" 
+                type="number"
+                onChange={(event) => {setProjectEstimatedTime(event.target.value)}}
+            /> 
+            <p className = "hours">Hours</p>
+        </article>
+        
+        <article className="formatting"> 
+            <p className = "labels">Member 1</p>
+            <input className = "member-input" placeholder="Enter a staff member"/>
+        </article>
+        
+        <article className="formatting"> 
+            <p className = "labels">Member 2</p>
+            <input className = "member-input" placeholder="Enter a staff member"/>
+        </article>
+        
+        <article className="formatting"> 
+            <p className = "labels">Member 3</p>
+            <input className = "member-input" placeholder="Enter a staff member"/>
+        </article>
+        
+        <article className="formatting"> 
+            <p className = "labels">Member 4</p>
+            <input className = "member-input" placeholder="Enter a staff member"/>
+        </article>
+        
+        <button className="create-project" onClick={handleButtonClick}> Add project</button>
+        
+    </section>
     );
 }
 
 
 const ManagerProjectPage = () => {
-    // Variable
-
+    // Variables
     const [viewProjects, setViewProjects] = useState(true);
-    const [projects, setProjects] = useState([]);
+    //TODO: Find a way to get a managers Employee_ID
+    const managerID = 5;
 
     // Functions & Logic
-    useEffect(() => {
-        getAllProjects()
-        .then((data) => {
-            setProjects(data)
-        })
-        .catch((errorMessage) => {
-            console.error(errorMessage);
-        });
-    }, []);
 
     //TODO: comment code!!!
     const ViewProjectsButtonClicked = () => {
@@ -72,8 +149,8 @@ const ManagerProjectPage = () => {
                 </section>
 
                 {viewProjects 
-                 ? <LoadViewProjectsSection projects={projects}/> 
-                 : <LoadAddProjectsSection/>
+                 ? <ViewProjectsSection managerID={managerID}/> 
+                 : <AddProjectsSection managerID={managerID} setViewProjects={setViewProjects}/>
                 }
                 
             </section>
