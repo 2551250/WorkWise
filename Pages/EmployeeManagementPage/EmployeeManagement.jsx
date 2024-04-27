732de60ba4f431ac382d9a0f38f351ddb403f693
YassirAwad <160495724+YassirAwad@users.noreply.github.com>
2024-04-27 17:13:22 +0000

    DIsplay Staff Product Page
    Displaying the View Product Page for  each Staff Member

---
+++ Pages/EmployeeManagementPage/EmployeeManagement.jsx
@@ -0,0 +1,64 @@
+import { React, useState, useEffect } from "react";
+
+import { getAllEmployees } from "../../backend";
+
+import Header from "../../Components/Header/Header";
+import EmployeeComponent from "../../Components/EmployeeComponent/EmployeeComponent";
+
+import "./EmployeeManagement.css";
+
+
+const getAllStaffData = (data) => {
+    /* 
+        Gets all the data on employees whose roles are Staff.
+        
+        :param data: json of all the data in Employee datatable
+        :return: An array filtered to contain staff data
+    */
+    return data.filter((employee) => employee.ROLE === "Staff"); //filteres data by staff;
+}
+
+
+const EmployeeManagement = () => {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        getAllEmployees()
+        .then((data) => {
+            const AllStaffData = getAllStaffData(data);
+            setEmployees(AllStaffData);
+        })
+        .catch((errorMessage) => {
+            console.error(errorMessage);
+        });
+    }, []);
+
+    return (
+        <>
+            <Header>
+                <h1>Employee Management</h1>
+                <button className="logoutButton">Log Out</button>
+            </Header>
+
+            <main className="list-wrapper">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th className="table-element">Name</th>
+                            <th className="table-element">Email</th>
+                            <th className="table-element">Change Permissions</th>
+                            <th className="table-element">Delete</th>
+                        </tr>
+                        {employees.map((employee) => (
+                            <EmployeeComponent employee={employee} key={employee.EMAIL} />
+                        )
+                        )}
+                    </tbody>
+                </table>
+            </main>
+        </>
+    );
+}
+
+export {getAllStaffData};
+export default EmployeeManagement;
---
+++ Pages/ProjectPages/HRProjectPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../../Components/Header/Header";
+
+const HRProjectPage = () => {
+    return (
+        <>
+            <Header>
+                <h1> Projects - HR </h1>
+            </Header>
+        </>
+    );
+}
+    
+export default HRProjectPage;
