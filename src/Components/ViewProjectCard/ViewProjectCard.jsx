f64dce7264acea792e33edbbff2e21ed30a225cc
NtandoRaji <2584925@students.wits.ac.za>
2024-05-18 14:37:14 +0200

    Updated Manager View Project Page

--- src/Components/ViewProjectCard/ViewProjectCard.jsx
+++ src/Components/ViewProjectCard/ViewProjectCard.jsx
@@ -1,50 +1,20 @@
 import React from "react";
 
 import "./ViewProjectCard.css";
-import { useNavigate } from "react-router";
 import expandIcon from "../../Assets/expand-icon.svg";
 
 // Component to display the information projects from a manager's perspective
-const ViewProjectCard = ({ projectID, name, description, estimatedTime, members, manager, managerID }) => {
-        // Project Data
-        const project = {
-            PROJECT_ID: projectID,
-            PROJECT_NAME: name, 
-            DESCRIPTION: description,
-            ESTIMATED_TIME: estimatedTime,
-            ASSIGNED_STAFF: members,
-            MANAGER: manager,
-            MANAGER_ID: managerID
-        }
-    
-    const navigate = useNavigate();
-
-    const setChatButton = () => {
-        // Use navigate function to go to another page
-        navigate('/ChatPage', {state: project});
-    };
+const ViewProjectCard = ({ project, onView}) => {
 
     return (
         <article>
             <table className="table-wrapper">
                 <tbody>
                     <tr>
-                    <td className="project-name">{name}</td>
-                    <td className="project-expand"><img src = {expandIcon}/></td>
-                    {/* <td className="project-desc">{description}</td>
-                    <td className="project-est-time">{estimatedTime} Hrs</td>
-                    <td className="project-members2">
-                        <ul>
-                            {
-                                members !== "Error"
-                                ? members.map((member) => (
-                                    <li key={member.EMPLOYEE_ID}> {`${member.NAME} ${member.SURNAME}`} </li>
-                                ))
-                                : ""
-                            }
-                        </ul>
+                    <td className="project-name">{project.PROJECT_NAME}</td>
+                    <td className="project-expand">
+                        <img src = {expandIcon} onClick={() => {onView(project)}} alt="project details"/>
                     </td>
-                    <td className="buttons-display"> <button className="time-button" onClick={setChatButton}> Group Chat </button></td> */}
                 </tr>
                 </tbody>
             </table>
