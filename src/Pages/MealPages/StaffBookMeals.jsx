e020826a15774a7357686ba5452e19086751c124
NtandoRaji <2584925@students.wits.ac.za>
2024-05-19 11:05:38 +0200

    Fixed merge conflicts

--- src/Pages/MealPages/StaffBookMeals.jsx
+++ src/Pages/MealPages/StaffBookMeals.jsx
@@ -85,7 +85,7 @@ const SelectMealSection = ({employeeID, bookedMeal, setBookedMeal, setActiveSect
             )}
         </main>
     );
-};
+}
 
 const BookMeals = () => {
     // Variables
@@ -125,17 +125,13 @@ const BookMeals = () => {
         navigate("/Staff");
     }
 
-    const logoutClicked = () =>{
-        navigate("/");
-    }
-
     // HTML Code
     return (
         <>
         <Header>
             <h1> Workwise </h1>
             <button className="homepage-button"  onClick={homePageButton}>Homepage</button>
-            <button className="logout-button" onClick={logoutClicked}>Log Out</button>
+            <button className="logout-button">Log Out</button>
         </Header>
 
         {activeSection === "viewBookedMealSection" && <BookedMealSection meal={bookedMeal}/> }
